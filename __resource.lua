a360ae0493359007d60a894c95e5987b8fb85f53
InjustableCustoms <skylineethany@gmail.com>
2022-03-28 20:35:46 +1030

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,10 @@
+--[[
+
+Simple script that toggles your car engine using the /eng command
+Seems to work fine with Realistic Vehicle Failure
+Made for Citylife:RP by Brickyard
+http://www.twitch.tv/brickyard_tv
+
+]]--
+resource_manifest_version 'f15e72ec-3972-4fe4-9c7d-afc5394ae207'
+client_script 'client.lua'
---
+++ client.lua
@@ -0,0 +1,54 @@
+local Keys = {
+  ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+  ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+  ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+  ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+  ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+  ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+  ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+  ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+  ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+--Setting initial variables
+engineRunning = false
+
+Citizen.CreateThread(function()
+  while true do --loop through the function
+    Citizen.Wait(100) --wait to prevent crashing
+    player = GetPlayerPed(-1)
+    if IsPedSittingInAnyVehicle(player) then --if the player is in a vehicle
+      vehicle = GetVehiclePedIsIn(player, false) --set the vehicle variable as the vehicle the player is in
+      if GetPedInVehicleSeat(vehicle, -1) == player then --and they are sitting in the driver seat
+        if GetIsVehicleEngineRunning(vehicle) then --and the engine is running
+          engineRunning = true --sets the variable used in the command /eng
+        else
+          engineRunning = false --sets the variable used in the command /eng
+        end
+      end
+    end
+
+  end
+end)
+
+--Turn on when attempting to move forward
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(0)
+    if IsControlJustReleased(0, Keys['W']) then --if the control is pressed
+      if not engineRunning then --if the engine is not running
+        SetVehicleEngineOn(vehicle, true, false, true) --turn it on
+      end
+    end
+  end
+end)
+
+--Register the command to toggle the engine
+RegisterCommand('eng', function()
+
+  if not engineRunning then --if the engine is not running, turn it on
+    SetVehicleEngineOn(vehicle, true, false, true)
+  else --if it is running, turn it off
+    SetVehicleEngineOn(vehicle, false, false, true)
+  end
+
+end, false)
